5d8d8472178c230136fc97a48a407484b69be880
Ratnaparise <144689824+Ratnaparise@users.noreply.github.com>
2023-10-04 19:54:49 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 #Launch an instance with 9000 and t2.medium
 cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
-unzip sonarqube-8.9.6.50800.zip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.2.50800.zip
+unzip sonarqube-9.9.2.50800.zip
 amazon-linux-extras install java-openjdk11 -y
 useradd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
